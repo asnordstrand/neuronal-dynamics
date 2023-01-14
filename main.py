7121efece1a3e2bc7fceaae1a70d736131ce96f2
Anna Sofie Nordstrand <annasofie@nordstrand.dev>
2023-01-14 07:07:09 -0600

    refactor: add LIF class
    Extract LIF model into a new class. The model now uses a step function
    to update its parameters for a given delta time.

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.subplots
 
+
+class LIF:
+    def __init__(self):
+        self.tau_m = 8.0  # Membrane time constant [ms]
+        self.abs_ref = 3.0  # Absolute refractory period [ms]
+        self.R_m = 2.6  # Membrane resistance [MΩ]
+        self.E_L = -70.0  # Leakage reversal potential [mV]
+        self.theta = -50.0  # Spiking threshold [mV]
+        self.time_from_spike = None  # Time elapsed from last spike [ms]
+
+        self.u = self.E_L  # Membrane potential [mV]
+        self.u_spike = 20.0  # Spiking decoration [mV]
+
+    def step(self, input_current, dt):
+        if self.time_from_spike is not None and self.time_from_spike < self.abs_ref:
+            self.u = self.E_L
+            self.time_from_spike += dt
+        elif self.u == self.u_spike:
+            self.u = self.E_L
+        elif self.u >= self.theta:
+            self.u = self.u_spike
+            self.time_from_spike = 0.0
+        else:
+            du = (-(self.u - self.E_L) + self.R_m * input_current) / self.tau_m
+            self.u += du * dt
+
+
 simulation_time = 1000  # Total simulation time [ms]
 dt = 0.01  # Time step [ms]
 time = np.arange(0, simulation_time, dt)  # Discrete time [ms]
@@ -10,39 +37,24 @@ theta = 0.03  # Rate of mean reversion
 mu = 8.0  # Long-term mean
 sigma = 0.9  # Noise amplitude
 
-x = np.zeros(len(time))  # Input current [nA]
+input_current = np.zeros(len(time))  # Input current [nA]
 eta = np.random.normal(0, 1, len(time))
 
 for t in range(len(time) - 1):
     # Numerical simulation of Ornstein-Uhlenbeck process with Euler-Maruyama method
-    x[t + 1] = x[t] + theta * (mu - x[t]) * dt + sigma * np.sqrt(dt) * eta[t]
+    input_current[t + 1] = input_current[t] + theta * (mu - input_current[t]) * dt + sigma * np.sqrt(dt) * eta[t]
 
-tau_m = 8.0  # Membrane time constant [ms]
-abs_ref = 3.0  # Absolute refractory period [ms]
-R_m = 2.6  # Membrane resistance [MΩ]
-E_L = -70.0  # Leakage reversal potential [mV]
-u_thresh = -50.0  # Spiking threshold [mV]
+neuron = LIF()
+u = []
 
-u = np.zeros(len(time))  # Membrane potential [mV]
-u[0] = E_L
-
-t_hat = 0.0  # Last spike time [ms]
-for t in range(len(time) - 1):
-    if t - t_hat < abs_ref / dt:
-        u[t] = E_L
-
-    if u[t] >= u_thresh:
-        u[t] = 20.0
-        u[t + 1] = E_L
-        t_hat = t
-    else:
-        du = (-(u[t] - E_L) + R_m * x[t]) / tau_m
-        u[t + 1] = u[t] + dt * du
+for t in range(len(time)):
+    neuron.step(input_current[t], dt)
+    u.append(neuron.u)
 
 fig = plotly.subplots.make_subplots(rows=2, cols=1)
 
 fig.add_trace(go.Scatter(x=time, y=u), row=1, col=1)
-fig.add_trace(go.Scatter(x=time, y=x), row=2, col=1)
+fig.add_trace(go.Scatter(x=time, y=input_current), row=2, col=1)
 
 fig.update_xaxes(title_text="t [ms]")
 fig.update_yaxes(title_text="Membrane potential [mV]", row=1, col=1)
